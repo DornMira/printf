cfca0ad535ec7d25c494010800235e5e4bf24c56
ElmarOdz <110916781+ElmarOdz@users.noreply.github.com>
2022-10-17 16:41:47 +0300

    Update _printf.c
     a function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -26,9 +26,17 @@ int _printf(const char *format, ...)
 		if (*p == '%')
 		{
 			p++;
-			if (*p == '%')                                                                  {                                                                                count += _putchar('%');                                                        continue;                                                                       }
-                        while (get_flag(*p, &flags))			                                p++
-			pfunc = get_print(*p);                                                          count += (pfunc)				                                ? pfunc(arguments, &flag)			                                : _printf("%%%c", *p);
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+                        while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
 		} else
 			count += _putchar(*p);
 	}
